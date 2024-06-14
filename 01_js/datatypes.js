6a593bbfceca9a223aa85b04089989a80c6caeba
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-14 07:02:23 +0000

    dt

--- 01_js/datatypes.js
+++ 01_js/datatypes.js
@@ -1,4 +1,15 @@
-let swami = 389426
+const swami = 389426
 console.log(swami);
+
+const abc = 4634
+console.log(typeof(abc));
+
+let myop1 = true
+console.log(typeof(myop1));
+
+myop2 = Number(myop1)
+console.log(typeof(myop2));
+console.log(myop1);
+
 
 
