85d1a6644caffc617105bcb24c72af2e78843c11
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-14 13:30:15 +0000

    string methods

---
+++ 01_js/srtings.js
@@ -0,0 +1,25 @@
+let one = "  my name is swami"
+console.log(one);
+console.log(one.length);
+console.log(one.toUpperCase);
+console.log(one.charAt(3));
+console.log(one.charCodeAt(5));
+console.log(one.at(5));
+console.log(one[2]);
+
+console.log(one.trimStart());
+console.log(one.slice(2,6));
+console.log(one.slice(2));
+console.log(one.slice(6));
+
+two = one.trim().replace("swami","worrier")
+console.log(two);
+console.log(one.substring(6,8));
+
+ok = "this is just text"
+console.log(one.concat(" ",ok));
+
+console.log(one.padStart(7,"0"));
+
+
+
