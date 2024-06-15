09364c85f567d2ef8f21224b6174ced453c6447f
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-15 08:53:35 +0000

    math

---
+++ 01_js/nums_math.js
@@ -0,0 +1,30 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+console.log(score);
+
+
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const otherNo = 84758975.5878
+console.log(otherNo.toPrecision(8))
+
+console.log(otherNo.toLocaleString('en-IN'))
+
+
+console.log(Math);
+console.log(Math.abs(-5634));
+console.log(Math.round(534.423));
+console.log(Math.ceil(3.532));
+console.log(Math.floor(34.5664));
+console.log(Math.min(3,4,5,6,8,5));
+console.log(Math.max(3,4,5,6,8,5));
+
+
+console.log(Math.random());                          //---------------always betn 0 to 1
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
