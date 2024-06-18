05438910dcf9045287bed7a78884654460592391
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-18 02:59:43 +0000

    arrays

---
+++ 01_js/time_dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.toUTCString());
+
+
+
+console.log(typeof(myDate));
+console.log(typeof(Date));
+console.log(typeof(let));
---
+++ 02_js/arrays.js
@@ -0,0 +1,22 @@
+// array today i finished it all
+const myArray = [5 , 45 , 54, 54, 45 ,"swami " ,"Chaudhari", [3 ,5 ,4 ,54]]
+console.log(myArray);
+console.log(myArray.push(0,27));
+console.log(myArray);
+console.log(myArray.pop());
+console.log(myArray);
+console.log(myArray.unshift(6));
+console.log(myArray);
+
+const newArray = myArray.join()
+console.log(newArray);
+
+console.log("----------------------------------------------------------------------------------------------")
+// slice and splice
+const arrayOne = [3,4,3,2,1,4,4,1,6,5,3]
+const arrayTwo = arrayOne.slice(1,6);
+console.log(arrayTwo);
+
+const arrayThree = arrayOne.splice(0,3)
+console.log(arrayThree);
+console.log(arrayOne);
