0f904ae10c181deaea93bf40d70821ad25d74f43
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-18 09:17:26 +0000

    obj

---
+++ 02_js/object.js
@@ -0,0 +1,10 @@
+// two ways to create object
+//singleton ----- 
+// object.create  1] this method called constructors
+
+
+//-------------------------------------------------------------------------------------//
+// 2] object literals 
+
+const jsUsers = {}
+console.log(typeof(jsUsers));
