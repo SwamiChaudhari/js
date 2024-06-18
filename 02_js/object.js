c0b161dff8aff29a82021d043fd45ed7516db960
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-18 15:43:26 +0000

    obj

--- 02_js/object.js
+++ 02_js/object.js
@@ -8,3 +8,30 @@
 
 const jsUsers = {}
 console.log(typeof(jsUsers));
+
+const mySym = Symbol("key1")
+
+const myObj = {
+    name : "Swami",
+    "full name" : "Swami chaudhari" ,
+    date : 16 ,
+    [mySym] : "key1",
+    add : "csn" ,
+    Email : "Swami1642004@gmail.com" ,
+    isLoggedIn : false 
+}
+
+Object.freeze(myObj)
+myObj.Email = "swamichaudhari@gmail.com"
+
+
+console.log(myObj.name);
+console.log(myObj.Email);
+console.log(myObj["date"]);
+console.log(myObj["full name"]);
+
+console.log(myObj[mySym])
+console.log(typeof[mySym])
+
+
+console.log(myObj)
