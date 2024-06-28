d58810c695afdd7ac27a8765665c366cbec4610b
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-06-28 06:32:23 +0000

    prob

--- 0_start/hello.js
+++ 0_start/hello.js
@@ -1,1 +1,15 @@
 console.log("hello")
+let price = 100000000000000
+
+console.log(`total price is ${price}`);
+console.log(6 < "A");
+ 
+// solved just simple problem that string is grater than 3 char and start with A
+
+let str = "Apple"
+
+if(str.length >= 3 && str[0] == "A"){
+    console.log("its a good string");
+}else{
+    console.log("bad")
+}
