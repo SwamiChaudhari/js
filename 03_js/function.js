14c5e30cb395749fde7613f93a3f41c97a08e5a3
Swami chaudhari <125657609+SwamiChaudhari@users.noreply.github.com>
2024-07-10 18:21:21 +0000

    sgdai

---
+++ 03_js/function.js
@@ -0,0 +1,16 @@
+function myName(){
+    console.log("s")
+    console.log("s")
+    console.log("s")
+    console.log("s")
+    console.log("s")
+
+}
+
+myName()
+
+function addition(num1 , num2){
+     console.log(num1 + num2 );
+}
+const result = addition(4,4);
+console.log(`result is ${result}`);
